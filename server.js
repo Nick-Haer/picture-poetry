e15f1e099f654cda9e959018a56334405a01a46c
Nick.Haer <nhaer123@gmail.com>
2020-02-24 19:59:41 -0600

    added redirect to index.html for working with react router on page reload

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, './client/public/index.html'), function(
+    err
+  ) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
 dbConnection();
 
 app.use(routes);
